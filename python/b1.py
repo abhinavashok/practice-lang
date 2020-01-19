26c14d17a7a63e050be2e78c60094e76a35aee17
abhinavashok <abhinavashok@live.in>
2020-01-19 23:17:17 +0530

    basics of arrays

---
+++ python/b1.py
@@ -0,0 +1,14 @@
+# Learning Arrays
+
+##Declare an array
+a=[3,4,5,6]
+
+#Iterate over the array using for loop
+for item in a:
+    print(item)
+
+# Print using indices
+print("a[3]=",a[3])
+
+# using negative indices,when we put negative indice it goes to 0-(indice) meaning the other side of the array.
+print("item=",a[-1])
